0f4d6d279127ccee023ad758fa7efa50fe257c3a
codingjv <codingjv@gmail.com>
2015-06-11 20:44:38 -0400

    add form

--- hello.rb
+++ hello.rb
@@ -1,5 +1,31 @@
 require 'sinatra'
 
-get '/' do
-  "Hello World!"
+
+get '/' do 
+	erb :message_form
 end
+
+=begin
+get '/' do
+'<form action="action_page.php">
+First name:<br>
+<input type="text" name="firstname" value="Mickey">
+<br>
+Last name:<br>
+<input type="text" name="lastname" value="Mouse">
+<br><br>
+<input type="submit" value="Submit">
+</form>'
+end
+=end
+
+=begin
+get '/' do
+   hello = ""
+	(1..100).each do |i|
+		hello  = hello + "Hello! world!"
+	end
+	return hello 
+#  "<i><h1>Hello World!</i></h1>"
+end
+=end
